a64629d458373227bae1fddfe56ed510ee6a6544
Rushikesh Patil <prushikeh@gmail.com>
2024-07-30 14:54:36 +0530

    x

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config()
 const { connectMongoDB } = require('./connect');
 const cookieParser = require('cookie-parser');
 const { checkForAuthentication, restrictTo } = require('./middlewares/auth');
